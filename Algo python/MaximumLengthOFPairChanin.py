04a44fa9b332096d705a5d7556b1c694bd7770a6
anwarsayyad <anwarsayyad2631@gmail.com>
2023-08-26 20:49:12 +0530

    Sat Daily leet code

---
+++ Algo python/MaximumLengthOFPairChanin.py
@@ -0,0 +1,10 @@
+def findLongestChain(self, pairs: list[list[int]]) -> int:
+        pairs.sort(key=lambda x : x[1])
+        cur,ans = float('-inf'),0
+
+        for pair in pairs:
+            if cur < pair[0]:
+                cur = pair[1]
+                ans += 1
+        
+        return ans
